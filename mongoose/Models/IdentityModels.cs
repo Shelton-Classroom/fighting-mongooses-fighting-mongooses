851bd7640722cbccf8be17e0d1b35338485e3732
Michael Bourlotos <mbourlotos5001@email.vccs.edu>
2022-02-21 22:16:47 -0500

    revert

--- mongoose/Models/IdentityModels.cs
+++ mongoose/Models/IdentityModels.cs
@@ -9,7 +9,6 @@ namespace mongoose.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -17,8 +16,6 @@ namespace mongoose.Models
             // Add custom user claims here
             return userIdentity;
         }
-        
-
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -27,11 +24,10 @@ namespace mongoose.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-   
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
-        
     }
 }
