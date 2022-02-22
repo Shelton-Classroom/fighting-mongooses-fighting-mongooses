4ec168cec68d7506c79a391dc1dd1e53853aa880
mbourlotos5001 <70112474+mbourlotos5001@users.noreply.github.com>
2022-02-21 22:55:12 -0500

    Revert "revert"
    This reverts commit 851bd7640722cbccf8be17e0d1b35338485e3732.

--- mongoose/Controllers/AccountController.cs
+++ mongoose/Controllers/AccountController.cs
@@ -155,6 +155,8 @@ namespace mongoose.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    UserManager.AddToRole(user.Id, "Employer");
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
