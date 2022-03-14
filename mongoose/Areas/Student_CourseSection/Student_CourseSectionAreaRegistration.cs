253f6172f787742885a8f57b1f2b9e4bf678459d
Gaelan Rose Geronimo <gar26731@email.vccs.edu>
2022-03-13 21:39:24 -0400

    Area added for Student_Course to be further expanded on

---
+++ mongoose/Areas/Student_CourseSection/Student_CourseSectionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace mongoose.Areas.Student_CourseSection
+{
+    public class Student_CourseSectionAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Student_CourseSection";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Student_CourseSection_default",
+                "Student_CourseSection/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
