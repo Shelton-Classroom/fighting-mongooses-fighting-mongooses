e1ebc49de39f0f7f7cffd2d4e063db1c2dbcec2b
Michael Bourlotos <mbourlotos5001@email.vccs.edu>
2022-03-14 10:47:31 -0400

    saved_intership scaffolding with controller and views

---
+++ mongoose/Areas/Saved_InternshipSection/Saved_InternshipSectionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace mongoose.Areas.Saved_InternshipSection
+{
+    public class Saved_InternshipSectionAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Saved_InternshipSection";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Saved_InternshipSection_default",
+                "Saved_InternshipSection/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
