f2d5300332559b3d882fe1de84db4fd31dc2db7c
awm2267 <70163798+awm2267@users.noreply.github.com>
2022-03-14 07:49:29 -0500

    added view to InstructorInternship/Create

---
+++ mongoose/Areas/CourseSection/Controllers/InstructorInternshipController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mongoose.Areas.CourseSection.Controllers
+{
+    public class InstructorInternshipController : Controller
+    {
+        // GET: CourseSection/InstructorInternship
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CourseSection/InstructorInternship/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CourseSection/InstructorInternship/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CourseSection/InstructorInternship/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseSection/InstructorInternship/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseSection/InstructorInternship/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CourseSection/InstructorInternship/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CourseSection/InstructorInternship/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
