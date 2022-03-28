0d57c98606396c618afd4e3bc3241cbfaa756b2c
Michael Bourlotos <mbourlotos5001@email.vccs.edu>
2022-03-28 09:53:07 -0400

    Changed employer open internships to exclude internships that are active

--- mongoose/Areas/EmployerSection/Controllers/EmployersController.cs
+++ mongoose/Areas/EmployerSection/Controllers/EmployersController.cs
@@ -67,7 +67,12 @@ namespace mongoose.Areas.EmployerSection.Controllers
             var userId = User.Identity.GetUserId();
             ViewBag.User = userId;
             ViewBag.EmployerId = db.Employers.Where(e => e.Id == userId);
-            var internships = db.Internships.Where(i => i.Employer.Id == userId);   //List of internships created by logged in employer m.b.
+            /*var internships = db.Internships.Where(i => i.Employer.Id == userId);*/   //List of internships created by logged in employer m.b.
+
+            // List of internships created by logged in employer, excluding Internships that have a matching studen_internship
+            var internships = db.Internships.Where(i => !db.Student_Internship.Any(s => s.InternshipId == i.InternshipId)); 
+
+
             ViewBag.Saved = db.Saved_Internship.ToList();
             ViewBag.Developer = "MB";
             return View(internships.ToList());
