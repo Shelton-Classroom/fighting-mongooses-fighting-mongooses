47385a2310b739c0e5fd1d739b17b0bfe56f5f84
mbourlotos5001 <70112474+mbourlotos5001@users.noreply.github.com>
2022-04-21 16:19:45 -0400

    update assembly for push

--- mongoose/Properties/AssemblyInfo.cs
+++ mongoose/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.20.2022.0")]
+[assembly: AssemblyVersion("4.21.2022.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
